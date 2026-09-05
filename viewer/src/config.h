34c91c1acd21511214a33cb43a1fabbf5d5caee0
Johan Henriksson <jhn98032@gmail.com>
2026-09-05 19:14:29 +0200

    Added license information

--- viewer/src/config.h
+++ viewer/src/config.h
@@ -1,3 +1,12 @@
+/*
+ * libmtreefile - 
+ * Copyright (C) 2026 Johan Henriksson johan[a]dexar.se
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ */
+
 #ifndef FILE__CONFIG_H
 #define FILE__CONFIG_H
 
